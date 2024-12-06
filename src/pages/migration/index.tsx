ece76e32af2ae54fffaeeebcd606db84fa0ee6dd
Mahdi-HF <mhf1377@gmail.com>
2024-12-06 16:15:27 +0330

    update index.tsx

--- src/pages/migration/index.tsx
+++ src/pages/migration/index.tsx
@@ -1,15 +1,11 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import { isMobile } from 'react-device-detect'
-
 import ActionSetter, { ActionTypes } from 'components/App/Migrate/ActionSetter'
 import HeaderBox from 'components/App/Migrate/HeaderBox'
 import { MigrationWrap } from 'context/Migration'
 import MigratedTable from 'components/App/Migrate/MigratedTable'
 import ClaimWrap from 'components/App/Migrate/ClaimWrap'
-// import Table from 'components/App/Migrate/Table'
-// import CardBox from 'components/App/Migrate/CardBox'
-// import MigrationHeader from 'components/App/Migrate/MigrationHeader'
 
 export const Container = styled.div`
   display: flex;
@@ -20,11 +16,9 @@ export const Container = styled.div`
 `
 
 const Wrapper = styled(Container)`
-  /* border-radius: 12px; */
   background: ${({ theme }) => theme.bg2};
   margin: 0 auto;
   width: clamp(250px, 90%, 1168px);
-  /* margin-top: 30px; */
 
   & > * {
     &:nth-child(4) {
@@ -51,33 +45,21 @@ export const getImageSize = () => {
 }
 
 export default function Migrate() {
-  // const { account } = useWeb3React()
   const [selected, setSelected] = useState<ActionTypes>(ActionTypes.CLAIM)
-  // const showModal = useMemo(() => {
-  //   return localStorage.getItem('migrationTermOfServiceSignature' + account?.toString()) ? false : true
-  // }, [account])
-  // const [isOpenReviewModal, toggleReviewModal] = useState(showModal && !!account)
-
-  // useEffect(() => {
-  //   if (account) toggleReviewModal(showModal)
-  // }, [account, showModal])
 
   return (
-    <>
-      <Container>
-        <MigrationWrap>
-          <HeaderBox />
-          <ActionSetter selected={selected} setSelected={setSelected} />
+    <Container>
+      <MigrationWrap>
+        <HeaderBox />
+        <ActionSetter selected={selected} setSelected={setSelected} />
 
-          <Wrapper>
-            {/* {selected === ActionTypes.EASY && <CardBox />} */}
-            {/* {selected === ActionTypes.MANUAL && <Table />} */}
-            {selected === ActionTypes.CLAIM && <ClaimWrap />}
-            {selected === ActionTypes.DASHBOARD && <MigratedTable setSelected={setSelected} />}
-          </Wrapper>
-        </MigrationWrap>
-      </Container>
-      {/* <ConfirmationModal isOpen={isOpenReviewModal} toggleModal={(action: boolean) => toggleReviewModal(action)} /> */}
-    </>
+        <Wrapper>
+          {/* {selected === ActionTypes.EASY && <CardBox />} */}
+          {/* {selected === ActionTypes.MANUAL && <Table />} */}
+          {selected === ActionTypes.CLAIM && <ClaimWrap />}
+          {selected === ActionTypes.DASHBOARD && <MigratedTable setSelected={setSelected} />}
+        </Wrapper>
+      </MigrationWrap>
+    </Container>
   )
 }
